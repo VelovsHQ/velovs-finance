c20a0a58bdf3173aaeafd5cef8feb9ae0c89c4b6
Senuka Chandunu <senukaacc@gmail.com>
2025-12-30 00:13:25 +0530

    feat: loading page #4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="min-h-screen w-full bg-black flex items-center justify-center">
       <h1 className="text-white text-[200px] font-black tracking-tighter leading-none">
-        Velovs<sup className="text-[80px] font-black ml-1">TM</sup>
+        Velovs<sup className="text-[55px] font-black ml-0 -top-23">T M</sup>
       </h1>
     </div>
   );
